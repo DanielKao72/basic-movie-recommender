72d9a14bd70b70bd439a37154fae0eae1e1d456f
Juan Daniel Kao Pech <a19201340@alumnos.uady.mx>
2025-03-31 11:13:14 -0600

    feat: :sparkles: add Movie_Reviewer class

---
+++ services/Movie_Reviewer.py
@@ -0,0 +1,45 @@
+import csv
+import os
+
+class Movie_Reviewer:
+
+    def add_user_review(username, movie_titles, ratings, report="user_reviews_report.csv"):
+        """
+        Add a review for the movie.
+        """
+        with open(report, mode='a', newline='') as file:
+            writer = csv.writer(file)
+            
+            if not os.path.isfile(report):
+                writer.writerow(["Username"] + movie_titles)
+                
+            writer.writerow([username] + ratings)
+                
+    def is_registered_user(username, report="user_reviews_report.csv"):
+        """
+        Check if the user is registered.
+        """
+        with open(report, mode='r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if row[0] == username:
+                    return True
+        return False
+    
+    def update_user_review(username, movie_title, rating, report="user_reviews_report.csv"):
+        """
+        Update the review for the movie.
+        """
+        with open(report, mode='r') as file:
+            reader = csv.reader(file)
+            rows = list(reader)
+        
+        for i, row in enumerate(rows):
+            if row[0] == username:
+                rows[i] = [username] + rating
+                break
+        
+        with open(report, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(rows)
+        
